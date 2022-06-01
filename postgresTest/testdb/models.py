dfd1523d9237456a165d19836b6574a33f231cc0
Victor Oliveira Barros do Nascimento <victoroliveirabarros@outlook.com>
2022-05-31 23:58:49 -0300

    Initial Commit

---
+++ postgresTest/testdb/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
